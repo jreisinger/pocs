0fdc445dd64d8710083dc124e9dda9140e2a7d9d
jozef <jozef.reisinger@gmail.com>
2024-02-14 17:42:34 +0100

    write down stuff I don't understand

--- crypto/shift-multibytekey/README.md
+++ crypto/shift-multibytekey/README.md
@@ -1,3 +1,5 @@
+Simple shift (Caesar) cipher that supports multibyte encryption keys (`shift` supports just single byte keys). Taken from https://github.com/bitfield/eg-crypto/.
+
 ```
 echo hello | go run ./cmd/encipher -key DEADBEEF | go run ./cmd/decipher -key DEADBEEF
 ```
--- crypto/shift-multibytekey/shift.go
+++ crypto/shift-multibytekey/shift.go
@@ -4,6 +4,11 @@ package shift
 func Encipher(plaintext []byte, key []byte) []byte {
 	ciphertext := make([]byte, len(plaintext))
 	for i, b := range plaintext {
+		// A mod B is the remainder that's left after dividing A by B as many
+		// times as you can. E.g. 5 mod 2 = 1. Modular arithmetic is sometimes
+		// called "clock arithmentic" because it wraps around like an analog
+		// clock; 12 hours later than 5 o'clock can't be 17 o'clock, it's 5
+		// o'clock again. To put it another way, 17 mod 12 = 5.
 		ciphertext[i] = b + key[i%len(key)]
 	}
 	return ciphertext
